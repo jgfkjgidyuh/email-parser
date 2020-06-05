40703b76105242e134465695ee410532ee1ca72c
vt92i <benidiktusvio@gmail.com>
2020-06-05 21:05:51 +0700

    updated code

---
+++ README.md
@@ -0,0 +1,7 @@
+# email-parser
+
+## Description
+This is simple tool yet useful to extract all email from combos.
+
+## Usage
+`python main.py -i [your input file here]`
--- main.py
+++ main.py
@@ -1,78 +1,44 @@
-from colorama import init, Fore
-import os
-import re
-import io
+# Version 0.7.1
+import argparse, sys, os, re
 
-emailList = []
-emailExtracted = []
-totalLines = 0
-totalExtracted = 0
+parser = argparse.ArgumentParser(description='Extract all email from combos', add_help=False)
+totalLines, totalExtracted = 0, 0
 
 
-def inHandler(file):
-    try:
-        data = io.open(file, "r", encoding="utf-8", errors="ignore")
-        checkExtension = re.search(r"[.]+txt", file)
-        if (data):
-            if(checkExtension):
-                extractEmail(data)
-            else:
-                print("invalid extension")
-    except PermissionError:
-        print("permission denied")
-    except (FileNotFoundError, OSError):
-        print("file not found")
+def getInputFile():
+    parser.add_argument('-i', metavar="input_file", type=str, dest="inputfile", required=True)
+    args = parser.parse_args().inputfile
+    if (os.path.exists(args) and os.path.isfile(args) and (os.path.splitext(args)[len(os.path.splitext(args)) - 1] == ".txt")):
+        return args
+    else:
+        sys.exit("Please, check your input file.")
 
 
-def extractEmail(checkedFile):
+def extractAllEmail(i):
     global totalLines, totalExtracted
-    emailList = checkedFile.readlines()
-    for email in emailList:
-        totalLines += 1
-        getEmail = re.search(
-            r"([a-z0-9_.+-]+[@]+[a-z0-9-]+[.]+[a-z.]+)", email, re.I)
-        if (getEmail):
-            emailExtracted.append(getEmail.group())
-            totalExtracted += 1
-    outFile(emailExtracted)
+    with open(file=i, mode="r", encoding="utf-8", errors="ignore") as f:
+        extracted = []
+        for email in f.readlines():
+            totalLines += 1
+            j = re.search(r"([a-z0-9_.+-]+[@]+[a-z0-9-]+[.]+[a-z.]+)", email, re.I)
+            if j:
+                extracted.append(j.group())
+                totalExtracted += 1
+    return extracted
 
 
-def outFile(emailExtracted):
-    out = open("out.txt", "w")
-    numIndex = 0
-    for email in emailExtracted:
-        out.write(email+"\n")
-        numIndex += 1
-        print("[{}] {}".format(numIndex, email))
-    out.close()
-    print("\nTotal lines: {}". format(totalLines))
-    print("Extracted email: {}\n". format(totalExtracted))
+def saveFile(o):
+    with open(file="out.txt", mode="w", encoding="utf-8") as f:
+        for item in o:
+            f.write(f"{item}\n")
+            print(item)
+        f.close()
+    print(f"\nTotal lines: {totalLines}")
+    print(f"Total extracted: {totalExtracted}\n")
 
 
-def main():
+if __name__ == "__main__":
     try:
-        os.system("clear")
-        appTitle = (Fore.RED+r"""
-                             _   _
-   ___   _ __ ___     __ _  (_) | |          _ __     __ _   _ __   ___    ___   _ __
-  / _ \ | '_ ` _ \   / _` | | | | |  _____  | '_ \   / _` | | '__| / __|  / _ \ | '__|
- |  __/ | | | | | | | (_| | | | | | |_____| | |_) | | (_| | | |    \__ \ |  __/ | |
-  \___| |_| |_| |_|  \__,_| |_| |_|         | .__/   \__,_| |_|    |___/  \___| |_|
-                                            |_|
-    | Author  : vt92i
-    | Version : 0.7
-    """)
-        print(appTitle)
-
-        inFile = input(
-            "{}Input file (*.txt) : {}".format(Fore.GREEN, Fore.WHITE))
-        if(inFile):
-            inHandler(inFile)
-
+        saveFile(extractAllEmail(getInputFile()))
     except KeyboardInterrupt:
-        print("\nCTRL + C")
-    except EOFError as e:
-        print(e)
-
-
-main()
+        sys.exit()
